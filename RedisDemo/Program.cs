e421f41a63f0dda5829fe2b1ba33397eee7a1e74
cailin0630 <903199067@qq.com>
2017-09-11 10:09:28 +0800

    import

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+namespace RedisDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch=new Stopwatch();
+            stopWatch.Start();
+            var key = new string[1000];
+            var value = new string[1000];
+            for (var i = 0; i < 1000; i++)
+            {
+               
+                key[i] = $"{i}";
+                value[i] = $"-{i}";
+
+                //SeRedisHelper.StringSet($"{i}", $"{i}{i}");
+                //Console.WriteLine($"set {i} {i}{i}");
+                //var result = SeRedisHelper.StringGet($"{i}");
+                //if (!result.IsSuccess)
+                //{
+                //    Console.WriteLine($"get {i} {result.Message}");
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"get {i} {result.Value}");
+                //}
+                
+            }
+            //SeRedisHelper.StringSetMany(key, value);
+            //SeRedisHelper.EntitySet("jy", new
+            //{
+            //    name="jiangyi",
+            //    age="26",
+            //    sex="男"
+            //},null,2);
+            var ret= SeRedisHelper.EntityGet<object>("jy", 2);
+            stopWatch.Stop();
+            Console.WriteLine($"{stopWatch.ElapsedMilliseconds}\n{ret}");
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ RedisDemo/SeRedisHelper.cs
@@ -0,0 +1,142 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using StandardResult4Net;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace RedisDemo
+{
+    /// <summary>
+    /// Redis操作类
+    /// </summary>
+    public class SeRedisHelper
+    {
+        private static readonly string Conn = ConfigurationManager.AppSettings["SERedis"] ?? "127.0.0.1:6379";
+
+        #region string类型
+
+        public static Result<string> StringGet(string key, int db = -1)
+        {
+            try
+            {
+                using (var client = ConnectionMultiplexer.Connect(Conn))
+                {
+                    var value = client.GetDatabase(db).StringGet(key);
+                    return Result<string>.Success(value);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result<string>.Fail(ex.Message);
+            }
+        }
+
+        public static Result<string[]> StringGetMany(string[] keyStrs, int db = -1)
+        {
+            try
+            {
+                var count = keyStrs.Length;
+                var keys = new RedisKey[count];
+                var addrs = new string[count];
+
+                for (var i = 0; i < count; i++)
+                {
+                    keys[i] = keyStrs[i];
+                }
+
+                using (var client = ConnectionMultiplexer.Connect(Conn))
+                {
+                    var values = client.GetDatabase(db).StringGet(keys);
+                    for (var i = 0; i < values.Length; i++)
+                    {
+                        addrs[i] = values[i];
+                    }
+                    return Result<string[]>.Success(addrs);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result<string[]>.Fail(ex.Message);
+            }
+        }
+
+        public static Result StringSet(string key, string value, int db = -1)
+        {
+            try
+            {
+                using (var client = ConnectionMultiplexer.Connect(Conn))
+                {
+                    var ret = client.GetDatabase(db).StringSet(key, value);
+                    return ret ? Result.Success() : Result.Fail("fail");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail(ex.Message);
+            }
+        }
+
+        public static Result StringSetMany(string[] keysStr, string[] valuesStr, int db = -1)
+        {
+            try
+            {
+                var count = keysStr.Length;
+                var keyValuePair = new KeyValuePair<RedisKey, RedisValue>[count];
+                for (var i = 0; i < count; i++)
+                {
+                    keyValuePair[i] = new KeyValuePair<RedisKey, RedisValue>(keysStr[i], valuesStr[i]);
+                }
+                using (var client = ConnectionMultiplexer.Connect(Conn))
+                {
+                    var ret = client.GetDatabase(db).StringSet(keyValuePair);
+                    return ret ? Result.Success() : Result.Fail("fail");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail(ex.Message);
+            }
+        }
+
+        #endregion string类型
+
+        #region 泛型
+
+        public static Result EntitySet<T>(string key, T t, TimeSpan? ts=null, int db = -1)
+        {
+            try
+            {
+                var str = JsonConvert.SerializeObject(t);
+                using (var client = ConnectionMultiplexer.Connect(Conn))
+                {
+                    var ret = client.GetDatabase(db).StringSet(key, str, ts);
+                    return ret ? Result.Success() : Result.Fail("fail");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail(ex.Message);
+            }
+        }
+
+        public static Result<T> EntityGet<T>(string key, int db = -1)
+            where T : class
+        {
+            try
+            {
+                using (var client = ConnectionMultiplexer.Connect(Conn))
+                {
+                    var strValue = client.GetDatabase(db).StringGet(key);
+                    return string.IsNullOrEmpty(strValue) ? Result<T>.Fail("redis return null") : Result<T>.Success(JsonConvert.DeserializeObject<T>(strValue));
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result<T>.Fail(ex.Message);
+            }
+        }
+
+        #endregion 泛型
+    }
+}
